91e911069269d4cc22c49726cf9422b5561ea4ad
Simon-Bakken-Jantasuk <94875981+Simon-Bakken-Jantasuk@users.noreply.github.com>
2022-03-25 22:19:28 +0100

    Create FinneEkstremalpunkter.py

---
+++ FinneEkstremalpunkter.py
@@ -0,0 +1,32 @@
+## Finne ekstremalpunktene til andregrads polynom.
+
+def f(x):
+    return x**2+10*x+25
+
+def fderivert(x):
+    return 2*x+10
+
+# while loop
+x = -10
+while x < 10
+    if fderivert(x) == 0:
+        print(x, f(x))
+    x = x + 1
+
+# Alternativ til while loop; så kan det lages en for loop.
+for x in range(-10,11):
+    if fderivert(x) == 0:
+        print(x, f(x))
+    x = x + 1
+# Mye finere...
+
+## Finne ekstremalpunktene til en polynom funksjon med flere ekstremalpunkter
+
+def f(x):
+  return (x + 1)*(x + 2)*(x + 4)
+
+def fderivert(x):
+  return 3*x**2 + 14*x + 14 
+ 
+# while loop
+x 
